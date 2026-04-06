cf478fc8d1fdd96c4913482c9036fda90893eefd
arvinn-boop <arvinn2006@gmail.com>
2026-04-06 14:19:48 -0700

    Add files via upload
    New lab 1 done

---
+++ lab_1.py
@@ -0,0 +1,31 @@
+print("Hello, Arvin!")
+
+print(5 + 6)
+
+length = 3
+width = 4
+area = length * width
+print("The area of a rectangle is", area)
+
+name = "Arvin"
+hobby = "working out"
+coding_experience = "beginner"
+weekly_hours_learn_coding = 5
+my_github_comp115_repo = "https://github.com/arvinnozari/comp115"
+
+print(f"""
+Hi,
+
+My name is {name}. I like {hobby}.
+
+My coding experience is {coding_experience}.
+I plan to spend about {weekly_hours_learn_coding} hours per week learning coding.
+
+Here is my COMP115 GitHub repository:
+{my_github_comp115_repo}
+
+Hope we have a great semester learning Python.
+
+Cheers,
+{name}
+""")
